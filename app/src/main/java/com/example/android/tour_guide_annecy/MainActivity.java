5f4e08704e23de3e3d9028600a709c1c00457384
Ashley <loxinchi@gmail.com>
2021-03-15 18:03:20 +0100

    delete doubled - setContentView in MainActivity.java

--- app/src/main/java/com/example/android/tour_guide_annecy/MainActivity.java
+++ app/src/main/java/com/example/android/tour_guide_annecy/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
